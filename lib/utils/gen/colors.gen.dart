3c2bdc950d43592cd63176e36310ffed99ee6fdc
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-08-03 20:10:11 +0900

    fix

---
+++ lib/utils/gen/colors.gen.dart
@@ -0,0 +1,18 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #024CBA
+  static const Color primary = Color(0xFF024CBA);
+}
